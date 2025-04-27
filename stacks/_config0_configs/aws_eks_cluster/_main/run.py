bf3607da56359a36bf1b4de316604530c3f5eece
gary <gary>
2025-04-27 14:42:14 +0800

    rendered and updated 04-27-2025-14-42

--- stacks/_config0_configs/aws_eks_cluster/_main/run.py
+++ stacks/_config0_configs/aws_eks_cluster/_main/run.py
@@ -127,7 +127,7 @@ def run(stackargs):
     stack.set_variable("build_image",
                        "aws/codebuild/standard:7.0")
 
-    stack.set_variable("build_timeout", 2700)
+    stack.set_variable("timeout", 800)
 
     # use the terraform constructor (helper)
     # but this is optional
@@ -165,7 +165,7 @@ def run(stackargs):
     if stack.get_attr("role_name"):
 
         inputargs = {
-            "build_timeout": stack.build_timeout,
+            "build_timeout": 900,
             "compute_type": stack.compute_type,
             "image_type": stack.image_type,
             "build_image": stack.build_image,
